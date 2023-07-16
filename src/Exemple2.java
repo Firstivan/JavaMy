6d80e19bc60c4d00d61141cc0c3857191ad522c2
Firstivan <Firstivan>
2023-07-17 00:18:01 +0300

    Проверка гида на IJ JDK

---
+++ src/Exemple2.java
@@ -0,0 +1,15 @@
+public class Exemple2 {
+    public static void main(String[] args) {
+        int myVar1;
+        int myVar2;
+
+        myVar1 = 1024;
+        System.out.println("Переменная myVar1 содережет: " + myVar1);
+
+        myVar2 = myVar1 / 2;
+
+        System.out.println("Переменная myVar2 содержет: ");
+        System.out.print(myVar2);
+
+    }
+}
